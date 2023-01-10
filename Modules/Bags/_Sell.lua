2f3cdcb98a399f4618c27acfc19eca17d0218023
dylanblokhuis <dylanblokhuis3@hotmail.com>
2023-01-10 22:25:28 +0100

    sell grey items automaticaly

---
+++ Modules/Bags/_Sell.lua
@@ -0,0 +1,18 @@
+local Module = BlokyUI:NewModule("Bags.Sell");
+
+-- all credis due to the author of SUI
+function Module:OnEnable()
+  local g = CreateFrame("Frame")
+  g:RegisterEvent("MERCHANT_SHOW")
+  g:SetScript("OnEvent", function()
+    local bag, slot
+    for bag = 0, 4 do
+      for slot = 0, C_Container.GetContainerNumSlots(bag) do
+        local link = C_Container.GetContainerItemLink(bag, slot)
+        if link and (select(3, GetItemInfo(link)) == 0) then
+          C_Container.UseContainerItem(bag, slot)
+        end
+      end
+    end
+  end)
+end
